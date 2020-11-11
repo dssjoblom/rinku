a6bbaec3e1d93c0227f72e8fbc41d513852468ac
Daniel Sjöblom <daniel@ziney.co>
2020-11-11 16:38:28 +0200

    Skip tag_options call

--- lib/rails_rinku.rb
+++ lib/rails_rinku.rb
@@ -7,24 +7,24 @@ module RailsRinku
     options = args.size == 2 ? {} : args.extract_options!
     unless args.empty?
       options[:link] = args[0] || :all
-      options[:html] = args[1] || {}
+      options[:html] = args[1] || ''
       options[:skip] = args[2]
     end
     options.reverse_merge!(:link => :all, :html => {})
     text = h(text) unless text.html_safe?
 
-    tag_options_method = if Gem::Version.new(Rails.version) >= Gem::Version.new("5.1")
-      # Rails >= 5.1
-      tag_builder.method(:tag_options)
-    else
-      # Rails <= 5.0
-      method(:tag_options)
-    end
+    # tag_options_method = if Gem::Version.new(Rails.version) >= Gem::Version.new("5.1")
+    #   # Rails >= 5.1
+    #   tag_builder.method(:tag_options)
+    # else
+    #   # Rails <= 5.0
+    #   method(:tag_options)
+    # end
 
     Rinku.auto_link(
       text,
       options[:link],
-      tag_options_method.call(options[:html]),
+      options[:html],
       options[:skip],
       &block
     ).html_safe
